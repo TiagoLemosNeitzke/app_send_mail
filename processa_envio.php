f7222ccf282e9d3ce6b4585d4525eb31f71283e0
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2022-05-14 17:03:52 -0400

    install PHPMailer and require

--- processa_envio.php
+++ processa_envio.php
@@ -1,5 +1,9 @@
 <?php
-//print_r($_POST);
+require "./bibliotecas/PHPMailer/Exception.php";
+require "./bibliotecas/PHPMailer/OAuth.php";
+require "./bibliotecas/PHPMailer/PHPMailer.php";
+require "./bibliotecas/PHPMailer/POP3.php";
+require "./bibliotecas/PHPMailer/SMTP.php";
 
 class Mensagem {
     private $para = null;
