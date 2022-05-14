51146bcd71c2ae9f0cc157c58ca772c11158c256
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2022-05-14 16:50:06 -0400

    first commit

---
+++ processa_envio.php
@@ -0,0 +1,40 @@
+<?php
+//print_r($_POST);
+
+class Mensagem {
+    private $para = null;
+    private $assunto = null;
+    private $mensagem = null;
+
+    public function __get($attr) {
+       return $this->$attr;
+    }
+
+    public function __set($attr, $valor) {
+        $this->$attr = $valor;
+    }
+
+    public function mensagemValida() {
+        //a function nativa do PHP empty() verifica se existe algum valor na variavel
+        if(empty($this->para) || empty($this->assunto || empty($this->mensagem))) {
+            return false;
+        }
+
+        return true;
+    }
+}
+
+$mensagem = new Mensagem();
+
+$mensagem->__set('para', $_POST['para']);
+$mensagem->__set('assunto', $_POST['assunto']);
+$mensagem->__set('mensagem', $_POST['mensagem']);
+
+//print_r($mensagem);
+
+if($mensagem->mensagemValida()) {
+    echo 'mensagem é válida';
+} else {
+    echo 'mensagem inválida';
+}
+?>
