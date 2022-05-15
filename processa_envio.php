820914e742281493a2d3a175957dce9e1e7e965a
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2022-05-15 07:21:14 -0400

    create page default return send mail

--- processa_envio.php
+++ processa_envio.php
@@ -8,22 +8,27 @@ require "./bibliotecas/PHPMailer/SMTP.php";
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-class Mensagem {
+class Mensagem
+{
     private $para = null;
     private $assunto = null;
     private $mensagem = null;
+    public $status = ['codigo_status' => null, 'descricao_status' => ''];
 
-    public function __get($attr) {
-       return $this->$attr;
+    public function __get($attr)
+    {
+        return $this->$attr;
     }
 
-    public function __set($attr, $valor) {
+    public function __set($attr, $valor)
+    {
         $this->$attr = $valor;
     }
 
-    public function mensagemValida() {
+    public function mensagemValida()
+    {
         //a function nativa do PHP empty() verifica se existe algum valor na variavel
-        if(empty($this->para) || empty($this->assunto || empty($this->mensagem))) {
+        if (empty($this->para) || empty($this->assunto || empty($this->mensagem))) {
             return false;
         }
 
@@ -38,10 +43,11 @@ $mensagem->__set('assunto', $_POST['assunto']);
 $mensagem->__set('mensagem', $_POST['mensagem']);
 
 // se a mesnagem não for válida entra no if abaixo, enterrompendo o script
-if(!$mensagem->mensagemValida()) {
+if (!$mensagem->mensagemValida()) {
     echo 'mensagem não é válida';
-    die(); //esta function nativa do PHP mata o processamento do script
-} 
+    //die(); //esta function nativa do PHP mata o processamento do script
+    header('Location: index.php');
+}
 
 //Cria uma instância; passando`true` habilitando exceções
 $mail = new PHPMailer(true);
@@ -55,29 +61,82 @@ try {
     $mail->Username   = 'webcompleto2@gmail.com';                     //SMTP username
     $mail->Password   = '!@#$4321';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       =587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
     $mail->setFrom('webcompleto2@gmail.com', 'Web completo remetente');
-    $mail->addAddress('webcompleto2@gmail.com', 'Web completo destinatário');     //Add a recipient
+    $mail->addAddress($mensagem->__get('para'));     //Add a recipient
     //$mail->addAddress('ellen@example.com');               //Name is optional
     //$mail->addReplyTo('info@example.com', 'Information'); //contato padrão caso o destinatário deseje responder o remetente
     //$mail->addCC('cc@example.com'); //adiciona destinatário de copia
     //$mail->addBCC('bcc@example.com'); //adiciona copia oculta
 
     //Attachments / anexos
-   // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-   // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Olá! eu sou o assunto';
-    $mail->Body    = 'Olá! eu sou o conteúdo do <b>e-mail!</b>';
-    $mail->AltBody = 'Olá! eu sou o conteúdo do e-mail!';
+    $mail->Subject = $mensagem->__get('assunto');
+    $mail->Body    = $mensagem->__get('mensagem');
+    $mail->AltBody = 'É necessário utilizar um client de e-mail que suporte HTML para ter acesso total ao conteúdo deste e-mail';
 
     $mail->send();
-    echo 'Message has been sent';
+
+    $mensagem->status['codigo_status'] = 1;
+    $mensagem->status['descricao_status'] = 'Seu e-mail foi enviado com sucesso!';
 } catch (Exception $e) {
-    echo "Não foi possível enviar este e-mail! Por favor tente novamente mais tarde. Detalhes do erro: {$mail->ErrorInfo}";
+    $mensagem->status['codigo_status'] = 2;
+    $mensagem->status['descricao_status'] = "Não foi possível enviar este e-mail! Por favor tente novamente mais tarde. Detalhes do erro: {$mail->ErrorInfo}";
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>App Send Mail</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="py-3 text-center">
+            <img class="d-block mx-auto mb-2" src="logo.png" alt="" width="72" height="72">
+            <h2>Send Mail</h2>
+            <p class="lead">Seu app de envio de e-mails particular!</p>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <? if ($mensagem->status['codigo_status'] == 1) { ?>
+                    <div class="container">
+                        <h1 class="display-4 text-success">Sucesso</h1>
+                        <p>
+                            <?= $mensagem->status['descricao_status'] ?>
+                        </p>
+                        <a href="index.php" class="btn btn-success btn-lg mt-5 text-white">Voltar</a>
+                    </div>
+
+                <? } ?>
+
+                <? if ($mensagem->status['codigo_status'] == 2) { ?>
+                    <div class="container">
+                        <h1 class="display-4 text-danger">Ops! Algo deu errado.</h1>
+                        <p>
+                            <?= $mensagem->status['descricao_status'] ?>
+                        </p>
+                        <a href="index.php" class="btn btn-danger btn-lg mt-5 text-white">Voltar</a>
+                    </div>
+
+                <? } ?>
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
